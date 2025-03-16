a6f48e1a6857825424223944efa6bd94f9fcdc16
Carter Brehm <carter@brehmit.com>
2025-03-16 14:25:06 -0500

    update EEPROM prototypes

--- Core/Inc/eeprom.h
+++ Core/Inc/eeprom.h
@@ -3,6 +3,10 @@
 
 #include "stm32f0xx_hal.h"
 
-void eeprom_test(void);
+void eeprom_write(uint8_t data, uint16_t address);
+void eeprom_write_multi(uint8_t *data, uint16_t address, uint16_t length);
+uint8_t eeprom_read(uint16_t address);
+void eeprom_read_multi(uint8_t *data, uint16_t address, uint16_t length);
+
 
 #endif
