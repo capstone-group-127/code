2dd7c76bf4bcf204f21232bb7483e24de722eb05
Carter Brehm <carter@brehmit.com>
2025-04-17 16:51:20 -0500

    modify EEPROM routines according to datasheet batch writes

--- Core/Src/eeprom.c
+++ Core/Src/eeprom.c
@@ -28,10 +28,25 @@ void eeprom_write(uint8_t data, uint16_t address) {
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
 }
 
-void eeprom_write_multi(uint8_t *data, uint16_t address, uint16_t length) {
-  for (uint16_t i = 0; i < length; i++) {
-    eeprom_write(data[i], address + i);
-  }
+void eeprom_write_batch(uint8_t *data, uint16_t address, uint16_t length) {
+  // Set the write enable latch
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(&hspi1, (uint8_t *)&CMD_WRITE_BIT, 1, 100);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+
+  HAL_Delay(100);
+  // Send the data
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(&hspi1, (uint8_t *)&CMD_WRITE, 1, 100);
+  HAL_SPI_Transmit(&hspi1, (uint8_t *)&address, 2, 100);
+  HAL_SPI_Transmit(&hspi1, data, length, 100);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+
+  HAL_Delay(100);
+  // Disable the write enable latch
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(&hspi1, (uint8_t *)&CMD_WRDI_BIT, 1, 100);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
 }
 
 uint8_t eeprom_read(uint16_t address) {
@@ -48,7 +63,10 @@ uint8_t eeprom_read(uint16_t address) {
 }
 
 void eeprom_read_multi(uint8_t *data, uint16_t address, uint16_t length) {
-  for (uint16_t i = 0; i < length; i++) {
-    data[i] = eeprom_read(address + i);
-  }
+  // Send the read command
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(&hspi1, (uint8_t *)&CMD_READ, 1, 100);
+  HAL_SPI_Transmit(&hspi1, (uint8_t *)&address, 2, 100);
+  HAL_SPI_Receive(&hspi1, data, length, 100);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
 }
